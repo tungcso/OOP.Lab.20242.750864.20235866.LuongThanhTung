ae638270cdcab6a23bedde324bc2f269f6d0f60a
tungcso <luongtung882005@gmail.com>
2025-03-01 09:14:43 +0700

    add Lab1

---
+++ Lab1/Code/FirstDialog.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class FirstDialog {
+
+public static void main(String[] args) {
+
+JOptionPane.showMessageDialog(null,"20235866 - Luong Thanh Tung, Hello World! How are you?");
+System.exit(0);
+}
+}
